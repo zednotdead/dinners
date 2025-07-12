eb74c409e7cf7b6c0ccd6369323bb8990e5328de
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-07-12 15:50:30 +0200

    avatar basics

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from '@/components/ui/button';
 export * from '@/components/ui/navigation-menu';
+export * from '@/components/ui/avatar';
 export * from '@/lib/utils';
--- frontend/src/components/login-indicator/index.tsx
+++ frontend/src/components/login-indicator/index.tsx
@@ -1,13 +1,20 @@
 import { AuthContext } from '@/context/auth';
-import { FC, useContext } from 'react';
+import { cn } from '@dinners/components';
+import { ComponentProps, FC, useContext } from 'react';
 
-export const LoginIndicator: FC = () => {
+export const LoginIndicator: FC<ComponentProps<'div'>> = ({ className, ...props }) => {
   const { user, logIn, logOut } = useContext(AuthContext);
-  return user
-    ? (
-        <div onClick={logOut}>{JSON.stringify(user)}</div>
-      )
-    : (
-        <button onClick={logIn}>Log in</button>
-      );
+  return (
+    <div className={cn('ml-auto', className)} {...props}>
+      {user
+        ? (
+            <div onClick={logOut}>
+              {JSON.stringify(user)}
+            </div>
+          )
+        : (
+            <button onClick={logIn}>Log in</button>
+          )}
+    </div>
+  );
 };
